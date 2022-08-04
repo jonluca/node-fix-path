0db9bc5b19f6824494a6f44dc1913a6c9208c3c3
JonLuca DeCaro <jonluca.decaro@gmail.com>
2022-08-04 15:10:52 -0400

    delete files

--- test.js
+++
@@ -1,9 +0,0 @@
-import process from 'node:process';
-import test from 'ava';
-import fixPath from './index.js';
-
-test('main', t => {
-	fixPath();
-
-	t.true(process.env.PATH.includes('/usr/local'));
-});
